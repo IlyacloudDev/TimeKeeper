88826958398ff6cb80bf74046ac25baf0d29b84e
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-10 21:22:02 +0300

    was registered overridden user model

--- TimeKeeper/users/admin.py
+++ TimeKeeper/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    """
+    Administrative panel for the model CustomUser
+    """
+    list_display = ('username', 'email', 'gender')
+    search_fields = ('username', 'email')
