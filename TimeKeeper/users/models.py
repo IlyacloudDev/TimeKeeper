b8b168acdc46ca914b3e1bf2f9e28479fa2efd5f
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-10 21:16:18 +0300

    was overridden AUTH_USER_MODEL

--- TimeKeeper/users/models.py
+++ TimeKeeper/users/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """
+    Custom model of User with extra avatar-field and gender-field.
+    """
+
+    female = 'FE'
+    male = 'MA'
+    GENDER_TYPES = [
+        (female, _('Female')),
+        (male, _('Male')),
+    ]
+
+    gender = models.CharField(
+        max_length=2,
+        choices=GENDER_TYPES,
+        default=female,
+        verbose_name=_('Gender of user')
+    )
+
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        blank=True,
+        verbose_name=_('Avatar of user')
+    )
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
