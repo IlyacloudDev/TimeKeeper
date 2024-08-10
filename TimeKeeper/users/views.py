19270390b3e70d0254faa4e1d3f2946a949d7892
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-11 01:16:27 +0300

    added view for updating user data

--- TimeKeeper/users/views.py
+++ TimeKeeper/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+from .models import CustomUser
+from .forms import CustomUserUpdateForm
 
-# This file will be updated later
+
+class CustomUserUpdate(LoginRequiredMixin, UpdateView):
+    """
+    View for updating user data
+    """
+
+    form_class = CustomUserUpdateForm
+    model = CustomUser
+    template_name = 'users/update.html'
+    success_url = reverse_lazy('test')
+
+    def get_object(self, queryset=None):
+        return self.request.user
