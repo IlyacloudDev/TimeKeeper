f6b0cc9afc1d118df298fe53bda94153c213194a
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-12 15:03:09 +0300

    added URL-path to deleting user's account

--- TimeKeeper/users/urls.py
+++ TimeKeeper/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomUserUpdate
+from .views import CustomUserUpdate, CustomUserDelete
 
 
 urlpatterns = [
     path('edit-account/', CustomUserUpdate.as_view(), name='account_edit'),
+    path('delete-account', CustomUserDelete.as_view(), name='account_delete'),
 ]
