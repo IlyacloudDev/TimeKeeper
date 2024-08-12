9fffdccacd503d623c09bd55f4bc82b827e1d545
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-12 15:01:40 +0300

    added view for deleting user's account with confirmation password; added changing the default avatar depending on gender

--- TimeKeeper/users/views.py
+++ TimeKeeper/users/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import authenticate
+# from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.utils.translation import gettext_lazy as _
 from django.urls import reverse_lazy
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, DeleteView
 from .models import CustomUser
-from .forms import CustomUserUpdateForm
+from .forms import CustomUserUpdateForm, PasswordConfirmationForm
 
 
 class CustomUserUpdate(LoginRequiredMixin, UpdateView):
@@ -17,3 +21,44 @@ class CustomUserUpdate(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+
+        # проверяем, пользователь изменил только пол в настройках пользователя или же ещё аватар
+        if ('gender' in form.changed_data) and ('avatar' not in form.changed_data):
+            # если пользователь изменил только пол, смотри, стоит ли у него дефолтная аватарка
+            if self.request.user.avatar.url in ['/media/default_avatars/female.jpg', '/media/default_avatars/male.jpg']:
+                # если стоит дефолтная аватарка, меняем на другую дефолтную аватарку в зависимости от пола
+                if self.request.user.avatar.url == '/media/default_avatars/female.jpg':
+                    self.request.user.avatar = 'default_avatars/male.jpg'
+                    user.save()
+                else:
+                    self.request.user.avatar = 'default_avatars/female.jpg'
+                    user.save()
+        return super().form_valid(form)
+
+
+class CustomUserDelete(LoginRequiredMixin, DeleteView):
+    """
+    View for deleting user account
+    """
+    model = CustomUser
+    form_class = PasswordConfirmationForm
+    template_name = 'users/delete.html'
+    success_url = reverse_lazy('start')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def form_valid(self, form):
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=self.request.user.username, password=password)
+        if user is not None:
+            # Удаляем пользователя
+            self.request.user.delete()
+            # Перенаправляем на success_url
+            return HttpResponseRedirect(self.success_url)
+        else:
+            form.add_error('password', _("Password incorrect. Please try again."))
+            return self.form_invalid(form)
