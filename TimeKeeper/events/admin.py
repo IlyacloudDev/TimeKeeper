3c8d94700958b837f8ba4f8cb7bb2bd17a76e23f
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-17 23:10:53 +0300

    registered model "Event"

--- TimeKeeper/events/admin.py
+++ TimeKeeper/events/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    """
+    Administrative panel for the model Event
+    """
+    list_display = ('title', 'created_at', 'updated_at', 'user')
+    search_fields = ('title',)
