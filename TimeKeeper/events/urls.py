acb390757324f8a636e3939a50d1058b73c1d406
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-17 23:15:56 +0300

    added URL-path to creating events

--- TimeKeeper/events/urls.py
+++ TimeKeeper/events/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import test
+from .views import test, EventCreate
 
 
 urlpatterns = [
     path('', test, name='test'),
+    path('create-event/', EventCreate.as_view(), name='event_create')
 ]
