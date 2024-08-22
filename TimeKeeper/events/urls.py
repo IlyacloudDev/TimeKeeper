cd1fe0ecfbf032b5d96a2c3508f885839c408401
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-22 23:47:48 +0300

    added new URL-s for EventDetail; EventList

--- TimeKeeper/events/urls.py
+++ TimeKeeper/events/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import test, EventCreate
+from .views import test, EventCreate, EventDetail, EventList
 
 
 urlpatterns = [
     path('', test, name='test'),
-    path('create-event/', EventCreate.as_view(), name='event_create')
+    path('create-event/', EventCreate.as_view(), name='event_create'),
+    path('events/', EventList.as_view(), name='event_list'),
+    path('detail-event/<str:hash_id>/', EventDetail.as_view(), name='event_detail'),
 ]
