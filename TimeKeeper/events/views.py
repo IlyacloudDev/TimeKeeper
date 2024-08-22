deccbbd241b7cc1626683485726cf9e57cf19832
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-22 23:46:50 +0300

    added 2 news views: to check the event in detail; to check the list of events

--- TimeKeeper/events/views.py
+++ TimeKeeper/events/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView, ListView
 from .models import Event, CustomUser
 from .forms import EventCreateForm
 
@@ -21,3 +21,26 @@ class EventCreate(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = CustomUser.objects.get(pk=self.request.user.id)
         return super().form_valid(form)
+
+
+class EventDetail(LoginRequiredMixin, DeleteView):
+    model = Event
+    template_name = 'events/detail.html'
+    context_object_name = 'event'
+
+    def get_object(self, queryset=None):
+        hash_id = self.kwargs.get('hash_id')
+        user = CustomUser.objects.get(pk=self.request.user.id)  # текущий аутентифицированный пользователь
+
+        event = Event.objects.get(hash_id=hash_id, user=user)
+
+        return event
+
+
+class EventList(LoginRequiredMixin, ListView):
+    model = Event
+    template_name = 'events/list.html'
+    context_object_name = 'events'
+
+    def get_queryset(self):
+        return Event.objects.filter(user=self.request.user)
