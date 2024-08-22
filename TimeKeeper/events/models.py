36b698d2f7c7af042f6f7ce48c92e8bd3ceb3e9f
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-22 23:42:06 +0300

    added a new field for storing the hashed id

--- TimeKeeper/events/models.py
+++ TimeKeeper/events/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from users.models import CustomUser
 from django.utils.translation import gettext_lazy as _
+import hashlib
+
+
+def generate_hash_id(pk):
+    """
+    Generates a hashed ID based on the primary key.
+    The SHA-256 algorithm is used.
+    """
+    return hashlib.sha256(str(pk).encode('utf-8')).hexdigest()
 
 
 class Event(models.Model):
@@ -14,12 +23,17 @@ class Event(models.Model):
         updated_at: date and time of updating event
         user: a foreign key for the "CustomUser" model with a one-to-many connection
     """
-
+    hash_id = models.CharField(max_length=64, unique=True, blank=True, null=True, verbose_name=_('Hashed ID'))
     title = models.CharField(max_length=100, verbose_name=_('Title of event'))
     content = models.TextField(verbose_name=_('Content of event'), help_text=_('You have ability use "Markdown"'))
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Date and time of creating'))
     updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Date and time of updating'))
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='events', verbose_name=_('User'))
+
+    def save(self, *args, **kwargs):
+        if not self.hash_id:
+            self.hash_id = generate_hash_id(self.id)
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = _('Event')
