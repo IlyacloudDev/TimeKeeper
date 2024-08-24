37ad070f60531625232d2a6e57679e2b815259c6
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-24 22:50:01 +0300

    some fixed mistakes in URL-configuration

--- TimeKeeper/events/urls.py
+++ TimeKeeper/events/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import test, EventCreate, EventDetail, EventList
+from .views import EventCreate, EventDetail, EventList
 
 
 urlpatterns = [
-    path('', test, name='test'),
     path('create-event/', EventCreate.as_view(), name='event_create'),
-    path('events/', EventList.as_view(), name='event_list'),
+    path('list-events/', EventList.as_view(), name='event_list'),
     path('detail-event/<str:hash_id>/', EventDetail.as_view(), name='event_detail'),
 ]
