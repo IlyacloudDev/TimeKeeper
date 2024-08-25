f17bbdbb7e40523f87c3a2a64065135d4f2ca11b
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-26 01:13:32 +0300

    added 2 new paths to: update event; delete event

--- TimeKeeper/events/urls.py
+++ TimeKeeper/events/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import EventCreate, EventDetail, EventList
+from .views import EventCreate, EventDetail, EventList, EventUpdate, EventDelete
 
 
 urlpatterns = [
     path('create-event/', EventCreate.as_view(), name='event_create'),
     path('list-events/', EventList.as_view(), name='event_list'),
     path('detail-event/<str:hash_id>/', EventDetail.as_view(), name='event_detail'),
+    path('update-event/<str:hash_id>/', EventUpdate.as_view(), name='event_update'),
+    path('delete-event/<str:hash_id>/', EventDelete.as_view(), name='event_delete')
 ]
