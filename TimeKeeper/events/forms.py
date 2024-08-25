26ee414a2c9b1c7fff6479a17bca4ba481022e22
Ilya Stritskiy <msv_ilya@mail.ru>
2024-08-26 01:15:23 +0300

    created form for confirm deleting of event

--- TimeKeeper/events/forms.py
+++ TimeKeeper/events/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Event
+from django.utils.translation import gettext_lazy as _
 
 
 class EventCreateForm(forms.ModelForm):
@@ -9,3 +10,13 @@ class EventCreateForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['title', 'content']
+
+
+class EventDeleteConfirmationForm(forms.Form):
+    """
+    Form for confirmation of event deleting
+    """
+    sentence = forms.CharField(
+        label=_("Repeat sentence below"),
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
